613d3b90fb02af1c5413b8e74ecc89d14e4c881e
Naeem <naeemkhandakar5@gmail.com>
2024-10-28 20:17:57 +0600

    1st practice problem solved

---
+++ problem_1.py
@@ -0,0 +1,9 @@
+# question
+# Write a Python program that takes a string input from the user and checks if Enjoy available in
+# the input string:
+
+
+# soln
+
+p=input("Enter the String : ")
+print("Enjoy" in p)
