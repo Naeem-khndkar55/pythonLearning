f84b43d08bf2a5ae9faab62541fc99ec900c7e2f
Naeem-khndkar55 <naeemkhandakar5@gmail.com>
2024-11-08 16:41:03 +0600

    removing the duplicate element from the string done

---
+++ problem_8.py
@@ -0,0 +1,13 @@
+# code for deleting the duplicate eliments from a string
+str = input("enter Your string: ")
+unique_element=set()
+res=""
+for c in str:
+    if c.isalpha():
+       if c not in unique_element:
+           unique_element.add(c)
+           res+=c
+
+
+
+print(f"the new string is: {res}")
